37af3f3376af65f113a3462324cc1476b22ff349
Anthony Leon <acidknights89@gmail.com>
2020-11-22 07:45:20 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,10 @@ class Show < ActiveRecord::Base
    belongs_to :network
    
    def actors_list
-     
-   end
+         self.characters.map do |c|
+             "#{c.actor.full_name}"
+         end
+     end
+
  
 end
